3d5857c5a36d338adf47f50b8fe06be0a899dcc6
KodaiMeboso-Java <muxiguangda@gmail.com>
2023-09-05 15:31:46 +0900

    例外クラス修正

--- src/main/java/com/example/mybatis0603/controller/NameController.java
+++ src/main/java/com/example/mybatis0603/controller/NameController.java
@@ -1,10 +1,7 @@
 package com.example.mybatis0603.controller;
 
 import com.example.mybatis0603.entity.Name;
-import com.example.mybatis0603.exception.ResourceNotFoundException;
 import com.example.mybatis0603.form.CreateForm;
-import jakarta.servlet.http.HttpServletRequest;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
@@ -12,7 +9,6 @@ import org.springframework.web.util.UriComponentsBuilder;
 import com.example.mybatis0603.service.AnimeService;
 
 import java.net.URI;
-import java.time.ZonedDateTime;
 import java.util.List;
 import java.util.Map;
 
@@ -33,19 +29,6 @@ public class NameController {
     public Name selectOneName(@PathVariable("id") int id) {
         return animeService.findById(id);
     }
-
-//    @ExceptionHandler(value = ResourceNotFoundException.class)
-//    public ResponseEntity<Map<String, String>> handleNoResourceFound(
-//            ResourceNotFoundException e, HttpServletRequest request) {
-//
-//        Map<String, String> body = Map.of(
-//                "timestamp", ZonedDateTime.now().toString(),
-//                "status", String.valueOf(HttpStatus.NOT_FOUND.value()),
-//                "error", HttpStatus.NOT_FOUND.getReasonPhrase(),
-//                "path", request.getRequestURI());
-//
-//        return new ResponseEntity<>(body, HttpStatus.NOT_FOUND);
-//    }
 
     @PostMapping("/names")
     public ResponseEntity<Map<String, String>> create(
